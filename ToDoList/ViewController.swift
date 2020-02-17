43a34098bc908610f4e8583a9c13df6e91e19d2b
Luis Güette <luis@infiniteautomation.com>
2020-02-16 21:55:00 -0400

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Luis Guette on 2/16/20.
+//  Copyright © 2020 Luis Guette. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
